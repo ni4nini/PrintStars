f0e03d76b51018cdbd8459568524bfd9ca75b65d
ni4nini <112921715+ni4nini@users.noreply.github.com>
2023-08-02 10:15:04 +0800

    加入專案檔案。

---
+++ PrintStars/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace PrintStars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btPrint = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rowChooser = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.rowChooser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btPrint
+            // 
+            this.btPrint.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btPrint.Location = new System.Drawing.Point(82, 217);
+            this.btPrint.Name = "btPrint";
+            this.btPrint.Size = new System.Drawing.Size(217, 72);
+            this.btPrint.TabIndex = 0;
+            this.btPrint.Text = "產生星星";
+            this.btPrint.UseVisualStyleBackColor = true;
+            this.btPrint.Click += new System.EventHandler(this.btPrint_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(76, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "列數：";
+            // 
+            // rowChooser
+            // 
+            this.rowChooser.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rowChooser.Location = new System.Drawing.Point(181, 115);
+            this.rowChooser.Name = "rowChooser";
+            this.rowChooser.Size = new System.Drawing.Size(120, 43);
+            this.rowChooser.TabIndex = 3;
+            this.rowChooser.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.rowChooser.ValueChanged += new System.EventHandler(this.rowChooser_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 362);
+            this.Controls.Add(this.rowChooser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btPrint);
+            this.Name = "Form1";
+            this.Text = "Print Starts";
+            ((System.ComponentModel.ISupportInitialize)(this.rowChooser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btPrint;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown rowChooser;
+    }
+}
+
